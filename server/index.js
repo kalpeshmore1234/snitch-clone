5a6781338d266e50517befbebed2ed2a00a7d567
Kalpesh More <90396306+kalpeshmore1234@users.noreply.github.com>
2024-01-03 21:12:53 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://127.0.0.1:27017/employee")
+mongoose.connect("mongodb://snitchclone.netlify.app:27017/employee")
 
 app.post("/login", (req, res) => {
    const {email, password} = req.body
